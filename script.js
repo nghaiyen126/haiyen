129c45e4ae38bec02f405374214a6f77a95c1b83
nghaiyen126 <haiyennguyen1267@gmail.com>
2025-02-21 10:18:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = tự đặt.value
+  if(u=="yen" && p=="haiyen")
+  {
+    window.open("home.html")
+    document getElementById(compiler).style.display="block"
+  }
+}
